8df2116ac46b666d7a3e31d8882db00f95992b6c
goran9999 <gutovicgoran@gmail.com>
2023-03-18 13:25:41 +0100

    updated env

--- typeorm.config.ts
+++ typeorm.config.ts
@@ -1,13 +1,15 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
 
+dotenv.config();
 export function typeormConfig(): TypeOrmModuleOptions {
   return {
     type: 'postgres',
-    host: 'derug-db',
-    username: 'dev',
-    password: 'dev',
-    port: parseInt('5432'),
-    database: 'dev',
+    host: process.env.POSTGRES_HOST,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    port: parseInt(process.env.POSTGRES_PORT),
+    database: process.env.POSTGRES_DB,
     entities: ['dist/**/*.entity.js'],
     synchronize: true,
     autoLoadEntities: true,
