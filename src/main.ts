b118968a10f7379a0425cd3d7c1e4d391bfbb70e
marsic3 <arsic.milos30@gmail.com>
2023-05-21 14:54:28 +0200

    enableCors

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: '*',
-  });
+  origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
+  optionsSuccessStatus: 204
+});
 
   await app.listen(3000);
   app.setGlobalPrefix('/api/v1');
