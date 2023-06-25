d0d4300d722213f73e1478212c22448ca70c628f
goran9999 <gutovicgoran@gmail.com>
2023-06-25 18:41:51 +0200

    switch to mainnet

--- src/utilities/solana/utilities.ts
+++ src/utilities/solana/utilities.ts
@@ -7,12 +7,16 @@ const CONNECTION_URL = process.env.SOLANA_ENDPOINT as string;
 
 dotenv.config();
 //TODO:load from env
-export const RPC_CONNECTION = new Connection('https://api.devnet.solana.com');
+export const RPC_CONNECTION = new Connection(
+  'https://mainnet.helius-rpc.com/?api-key=05a3a206-18c8-492f-bc34-9bff0beccaf2',
+);
 
 export const metaplex = new Metaplex(RPC_CONNECTION).use(
   bundlrStorage({
-    address: 'https://devnet.bundlr.network',
-    providerUrl: 'https://api.devnet.solana.com',
+    address:
+      'https://mainnet.helius-rpc.com/?api-key=05a3a206-18c8-492f-bc34-9bff0beccaf2',
+    providerUrl:
+      'https://mainnet.helius-rpc.com/?api-key=05a3a206-18c8-492f-bc34-9bff0beccaf2',
     timeout: 60000,
   }),
 );
